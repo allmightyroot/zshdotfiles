2d80ee0142f0a17f49a86241e45538ac2b864da5
allmightyroot <allmightyroot@gmail.com>
2017-04-28 14:42:46 -0500

    Add files via upload

---
+++ beasonaliases.zsh
@@ -0,0 +1,18 @@
+# Beason's ZSH Aliases
+alias ssh="ssh -q"
+alias ls="ls -h --color=auto"
+alias docker="sudo docker"
+
+# Luks stuff
+alias secbkopen="sudo cryptsetup luksOpen backups.lk securebackup"
+alias secbkclose="sudo cryptsetup luksClose securebackup"
+alias secbkmount="sudo mount /data/securebackup"
+alias secbkumount="sudo umount /data/securebackup"
+
+# SSHFS stuff
+alias nfshomemount="sshfs mapls090:/home/g035006 /data/nashome"
+alias nfshomeumount="fusermount -u /data/nashome"
+
+# Misc
+alias geturl='wget -r --no-clobber --page-requisites --html-extension --convert-links'
+alias wx='curl wttr.in/MSP'
