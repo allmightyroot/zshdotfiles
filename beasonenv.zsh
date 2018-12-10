d370eb7af3517865d75f445547b59e2092f4a0a9
allmightyroot <allmightyroot@gmail.com>
2018-12-10 12:41:27 -0600

    Adding ubuntu check to force python3.

--- beasonenv.zsh
+++ beasonenv.zsh
@@ -1,11 +1,18 @@
 # Python Virtual Environments (defaulting to python3)
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON="$(command \which python3)"
+OS=$(lsb_release -si)
 
 if [ -f "/etc/redhat-release" ];
 then
         export VIRTUALENVWRAPPER_VIRTUALENV="virtualenv-3.5"
 fi
+
+if [ "$OS" = "Ubuntu" ]; 
+then
+        export VIRTUALENV_PYTHON="/usr/bin/python3"
+fi
+      
 
 # Move vagrant home outside of /home
 export VAGRANT_HOME=/data/vagrant
