7872ac01b84641bb5eea232d4cbd52526dd09d5e
allmightyroot <allmightyroot@gmail.com>
2019-03-11 14:00:28 -0500

    Typo and shellcheck fixes.

--- beasonenv.zsh
+++ beasonenv.zsh
@@ -1,16 +1,20 @@
+#!/bin/zsh 
+
 # My GPG Key ID
 export KEYID=0x2f580f2d4315f522
 
 # Python Virtual Environments (defaulting to python3)
 export WORKON_HOME=$HOME/.virtualenvs
-export VIRTUALENVWRAPPER_PYTHON="$(command \which python3)"
+VIRTUALENVWRAPPER_PYTHON="$(command \which python3)"
+export VIRTUALENVWRAPPER_PYTHON
+
 OS=$(lsb_release -si)
 
 if [ "$OS" = "Fedora" ];
 then
         VIRTUALENV_PYTHON="/usr/bin/python3"
         GPG_TTY="$(tty)"
-        SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)VIRTUALENV_PYTHON
+        SSH_AUTH_SOCK=$(gpgconf --list-dirs agent-ssh-socket)
         export VIRTUALENV_PYTHON GPG_TTY SSH_AUTH_SOCK
         gpgconf --launch gpg-agent
 fi
