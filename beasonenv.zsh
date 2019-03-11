416f62b33c8f96859727f7986ae9061e45532654
allmightyroot <allmightyroot@gmail.com>
2019-03-11 13:25:20 -0500

    Adding KEYID with my gpg key.

--- beasonenv.zsh
+++ beasonenv.zsh
@@ -1,3 +1,6 @@
+# My GPG Key ID
+export KEYID=0x2f580f2d4315f522
+
 # Python Virtual Environments (defaulting to python3)
 export WORKON_HOME=$HOME/.virtualenvs
 export VIRTUALENVWRAPPER_PYTHON="$(command \which python3)"
